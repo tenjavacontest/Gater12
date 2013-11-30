c297bac13329d301ab619d887c53d503f2b4c25a
Bob Paulowaski <mcraftworldmc@gmail.com>
2013-11-30 09:50:07 -0800

    Edits

---
+++ tenjava/src/com/gmail/mcraftworldmc/tencontest/contest.java
@@ -0,0 +1,13 @@
+package com.gmail.mcraftworldmc.tencontest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+/**
+ * 
+ * @author MineOCraftMC (Gater12)
+ * @version 1.0
+ *
+ */
+
+public class contest extends JavaPlugin{
+
+}
